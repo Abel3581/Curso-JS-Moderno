62d608780a3f165db743000d308c40f363d01dd9
Abel3581 <marzoa3581@gmail.com>
2021-01-03 13:22:00 -0300

    Primera subida

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,8 @@
+// pido el nombre con promp
+
+const nombre = prompt('Cual es tu nombre')
+
+// innerHTML cambia contenido
+
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo javascript moderno`;
+
---
+++ 05-Operadores/js/02-app.js
@@ -0,0 +1,17 @@
+// Revisar si 2 valores son iguales o diferentes
+console.log(numero1 == numero3);
+console.log(numero1 == numero2);
+
+
+// Typeof
+console.log( typeof numero1 );
+console.log( typeof numero2 );
+
+// Operador estricto (revisa valor y tipo de dato)
+console.log(numero1 === numero2 );
+
+// Diferente a 
+
+console.log(numero1 != numero3);
+console.log(numero1 != numero2);
+console.log(numero1 !== numero2);
